9fdd4df6f89f55e8ed57216f444137ee8a3fa85c
Maryam Elkadi <119524599+MaryamElkadi@users.noreply.github.com>
2025-01-20 10:28:07 -0800

    Add files via upload

---
+++ Assignment1/mainToStudents.cpp
@@ -0,0 +1,42 @@
+int main()
+{
+    int len,wid,col,col2;
+    char boxChar;
+    cout<<"Enter length and width of the box separated by a space: ";
+    cin>>len>>wid;
+    ColoredBox* cb1;
+    cb1 = new ColoredBox(len,wid);
+    cb1->display();
+    cout<<"Set the box to another color: ";
+    cin>>col;
+    cb1->setColor(col);
+    cout<<"Displaying Transposed: "<<endl;
+    cb1->displayTransposed();
+    cout<<"Displaying Wider: "<<endl;
+    cb1->displayWider();
+    cout<<"Displaying Chess, enter the other color: "<<endl;
+    cin>>col2;
+    cb1->displayChess(col2);
+    cout<<endl<<"Area="<<cb1->getArea();
+    cout<<endl<<"Max Area="<<cb1->getMaxArea();
+    delete cb1;
+
+    cout<<"Enter length,width,color,character of the box separated by spaces: ";
+    cin>>len>>wid>>col>>boxChar;
+    ColoredBox* cb2;
+    cb2 = new ColoredBox(len,wid,col,boxChar);
+    cb2->display();
+    cout<<"Displaying Transposed: "<<endl;
+    cb2->displayTransposed();
+    cout<<"Displaying Wider: "<<endl;
+    cb2->displayWider();
+    cout<<"Displaying Chess, enter the other color: "<<endl;
+    cin>>col2;
+    cb2->displayChess(col2);
+    cout<<"Displaying original again:"<<endl;
+    cb2->display();
+    cout<<endl<<"Area="<<cb2->getArea();
+    cout<<endl<<"Max Area="<<cb2->getMaxArea();
+    delete cb2;
+    return 0;
+}
